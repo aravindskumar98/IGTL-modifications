9838668dcc3ada5d5b284c9d0fc0549cb9a87caa
venkat8900 <venkatmukthineni@gmail.com>
2022-05-04 19:17:06 -0400

    obtain names of pre initialised  nodes

--- src/parameter_event_handler.cpp
+++ src/parameter_event_handler.cpp
@@ -8,18 +8,25 @@ public:
     SampleNodeWithParameters()
         : Node("node_with_parameters")
     {
-        this->declare_parameter("an_int_param", 0);
+        const auto node_graph_interface = this->get_node_graph_interface();
+        std::vector<std::string> node_names = node_graph_interface->get_node_names();
 
+        for(auto existing_node_name : node_names){
+            RCLCPP_INFO(this->get_logger(),"Node Name : %s",existing_node_name.c_str());
+        }
+
+        this->declare_parameter("an_int_param", 0);
+        
         // Create a parameter subscriber that can be used to monitor parameter changes
         // (for this node's parameters as well as other nodes' parameters)
         param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
 
         auto cb = [this](const rcl_interfaces::msg::ParameterEvent &event)
-        {
+        {   
+            RCLCPP_INFO(this->get_logger(), "new event!");
             auto params = rclcpp::ParameterEventHandler::get_parameters_from_event(event);
             for (auto &p : params)
             {
-                std::cout << "here";
                 RCLCPP_INFO(this->get_logger(), "cb3: Received an update to parameter \"%s\" of type: %s: %s",
                             p.get_name().c_str(), p.get_type_name().c_str(), p.value_to_string().c_str());
             }
